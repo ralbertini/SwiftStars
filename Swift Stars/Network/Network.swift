41376f599a6e1c3bbcce191f4f815df246588fe9
Ronaldo Albertini <ronaldo.albertini@omni.com.br>
2019-08-28 22:12:52 -0300

    Adicionado Network

--- Swift Stars/Network/Network.swift
+++ Swift Stars/Network/Network.swift
@@ -8,6 +8,35 @@
 
 import UIKit
 
+enum httpMethod:String {
+    case get = "GET"
+    case post = "POST"
+    case delete = "DELETE"
+}
+
+enum NetworkErrors: Error {
+    case invalidURL
+}
+
 class Network: NSObject {
 
+    static var session: URLSession = {
+        
+        let session = URLSession(configuration: URLSessionConfiguration())
+        return session
+    }()
+    
+    
+    static func doRequest(url:String, httpMethod: httpMethod, params:Dictionary<String,Any>?, completionHandler:  @escaping (Data?, URLResponse?, Error?) -> Void) {
+        
+        guard let url = URL(string: url) else { return }
+        
+        let task = self.session.dataTask(with: url) { (data, response, error) in
+            
+            completionHandler(data, response, error)
+        }
+        
+        task.resume()
+        
+    }
 }
